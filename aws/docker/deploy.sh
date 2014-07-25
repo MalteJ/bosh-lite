f748a6bd15ce705e26527ef5a057872e92a1d2c9
Malte Janduda <mail@janduda.net>
2014-07-25 15:43:48 +0200

    fixing docker aws

--- aws/docker/deploy.sh
+++ aws/docker/deploy.sh
@@ -1,23 +1,30 @@
-#!/bin/bash
+#!/bin/bash --login
 echo "Deploying BOSH Lite Release"
 set -e
 set -x
 
-function finish {
+function destroy {
   cd /bosh-lite/aws
   vagrant destroy -f
 }
-trap finish EXIT
 
-cp /mnt/$BOSH_AWS_PRIVATE_KEY ~/id_rsa-vagrant
-chown root ~/id_rsa-vagrant
-export BOSH_LITE_PRIVATE_KEY=~/id_rsa-vagrant
+trap destroy EXIT SIGHUP SIGINT SIGTERM
+
+bosh --version
+
+cp /bosh-environment/tests/aws/$BOSH_AWS_PRIVATE_KEY ~/id_rsa_bosh
+chown root ~/id_rsa_bosh
+export BOSH_LITE_PRIVATE_KEY=~/id_rsa_bosh
 
 cd /bosh-lite/aws
 
+set +x
+echo "vagrant up --provider=aws"
 vagrant up --provider=aws | tee vagrantup.out
+set -x
 export DIRECTORIP=`cat vagrantup.out | grep "bosh target" | cut -d, -f1 | rev | cut -d' ' -f1 | rev`
 
+sleep 60
 bosh --non-interactive target $DIRECTORIP
 bosh login admin admin
 
@@ -25,7 +32,12 @@ bosh upload stemcell http://bosh-jenkins-gems-warden.s3.amazonaws.com/stemcells/
 
 export DIRECTORUUID=`bosh status | grep UUID | cut -dD -f2 | tr -d ' '`
 
-cd /bosh-deployment
-sed -i "s/.*director_uuid.*/director_uuid: $DIRECTORUUID/" *.yml
-bosh deployment *.yml
-bosh deploy
+cp -ra /bosh-environment /bosh-environment.local
+chown -R root /bosh-environment.local
+find /bosh-environment.local -type f -name "*.yml" -print0 | xargs -0 sed -i "s/.*director_uuid.*/director_uuid: $DIRECTORUUID/"
+
+cd /bosh-environment.local
+tests/$BOSH_TEST
+
+trap - EXIT INT SIG
+destroy
